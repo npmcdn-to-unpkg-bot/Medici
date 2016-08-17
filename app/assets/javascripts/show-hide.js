a18f0602f41ac5432a5537454877fd60ba8bd523
PeterDrakeNichols <PeterDrakeNichols@gmail.com>
2016-08-17 15:36:22 -0500

    museum row styling

---
+++ app/assets/javascripts/show-hide.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#museum-toggle").click(function(){
+        $(".museum-panel").fadeToggle();
+    });
+     $("#exhibition-toggle").click(function(){
+        $(".exhibition-panel").fadeToggle();
+    });
+});
