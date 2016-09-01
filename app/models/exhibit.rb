737ffa28d3b18e460b392dbfba8f6496f69435b6
scottsouthard <scottmsouthard@gmail.com>
2016-09-01 16:17:32 -0500

    Complete exhibit functionality

--- app/models/exhibit.rb
+++ app/models/exhibit.rb
@@ -4,4 +4,6 @@ class Exhibit < ApplicationRecord
   has_many :tags, through: :taggings
   accepts_nested_attributes_for :taggings, :allow_destroy => true
 
+  has_attached_file :photo, styles: { large: "768x768>", medium: "300x300>", thumb: "100x100>" }
+  validates_attachment_content_type :photo, content_type: /\Aimage\/.*\Z/
 end
